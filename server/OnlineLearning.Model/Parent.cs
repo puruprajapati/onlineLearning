7d9df7ebac2632d02175b19c5453bbf9ef50c8fd
pprajapati <prajapati.puru@gmail.com>
2020-05-30 14:33:07 +0545

    merged

---
+++ server/OnlineLearning.Model/Parent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnlineLearning.Model
+{
+    public class Parent
+                : BaseEntity
+    {
+        public string ParentName { get; set; }
+        public String Address { get; set; }
+        public String PrimaryContactNumber { get; set; }
+        public String SecondaryContactNumber { get; set; }
+        public String EmailAddress { get; set; }
+    }
+}
---
+++ server/OnlineLearning.Model/SessionStatus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnlineLearning.Model
+{
+    public class SessionStatus
+                    : BaseEntity
+    {
+        public String SessionStatusDesc { get; set; }
+
+    }
+}
