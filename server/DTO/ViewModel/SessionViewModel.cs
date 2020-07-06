c2cb5a0575bae64b343132480c4014c8875dfa6b
RoShan Shrestha <roshan7230@gmail.com>
2020-07-07 00:27:38 +0545

    Added Session id VM

--- server/DTO/ViewModel/SessionViewModel.cs
+++ server/DTO/ViewModel/SessionViewModel.cs
@@ -4,19 +4,20 @@ using System.Text;
 
 namespace OnlineLearning.DTO.ViewModel
 {
-  public class SessionViewModel
-  {
-    public String SessionTitle { get; set; }
-    public String SessionDesc { get; set; }
-    public Guid ClassId { get; set; }
-    public String ClassName { get; set; }
-    public Guid TeacherId { get; set; }
-    public String TeacherName { get; set; }
-    public DateTime ScheduledDate { set; get; }
-    public String StartingTime { get; set; }
-    public String EndingTime { get; set; }
-    public Guid? SessionStatusId { get; set; }
+    public class SessionViewModel
+    {
+        public Guid Id { get; set; }
+        public String SessionTitle { get; set; }
+        public String SessionDesc { get; set; }
+        public Guid ClassId { get; set; }
+        public String ClassName { get; set; }
+        public Guid TeacherId { get; set; }
+        public String TeacherName { get; set; }
+        public DateTime ScheduledDate { set; get; }
+        public String StartingTime { get; set; }
+        public String EndingTime { get; set; }
+        public Guid? SessionStatusId { get; set; }
 
-    public String SessionStatus { get; set; }
-  }
+        public String SessionStatus { get; set; }
+    }
 }
